f8c4158c710cf73b1a5a0a5d494d276e28f5ffa1
Bolone sengkuni <96203368+Bolone-sengkuni@users.noreply.github.com>
2024-10-03 06:27:37 +0700

    modified app.py in .

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ async def create_task(data: Data):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True, workers=4)
+    uvicorn.run("app:app", host="0.0.0.0", port=8001, reload=True, workers=4)
